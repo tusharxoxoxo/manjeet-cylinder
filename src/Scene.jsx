1181b8e839f75bdcacb31bba9dba3c608170d9f2
tusharxoxoxo <tusharxoxoxo@gmail.com>
2024-12-18 11:24:36 +0530

    video

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -6,7 +6,7 @@ import { useFrame } from '@react-three/fiber';
 
 export const Scene = () => {
 //    let tex = useTexture("./../smallestai.jpeg");
-let tex = useTexture("./manjeet.jpeg");
+let tex = useTexture("./../smallestai.jpeg");
     //    tex.needsUpdate = true;
     let scene = useRef(null);
     useFrame((state, delta) => {
@@ -17,7 +17,7 @@ let tex = useTexture("./manjeet.jpeg");
     return (
         <group rotation={[0, 1.4, 0.3]}>
             <mesh ref={scene} >
-                <cylinderGeometry args={[2, 2.5, 2, 60, 60, true]} />
+                <cylinderGeometry args={[1.5, 1.5, 2, 60, 60, true]} />
                 <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
             </mesh>
         </group>
